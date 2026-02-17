e4d3bb6be1c9707ee150369847a6f2fcbeb0b970
Benya-pha <128789537+Benya-pha@users.noreply.github.com>
2026-02-18 01:08:34 +0700

    Create Person class with constructor and introduce method

---
+++ LAB_6-Polymorphism/Person/Person.java
@@ -0,0 +1,19 @@
+package Person;
+
+public class Person {
+    String name;
+    int bornYear;
+
+    public Person() {
+    }
+
+    public Person(String name, int bornYear) {
+        this.name = name;
+        this.bornYear = bornYear;
+    }
+
+    public void introduce(){
+        System.out.println("My name is " + name + ".");
+        System.out.println("I was born in " + bornYear);
+    }
+}
