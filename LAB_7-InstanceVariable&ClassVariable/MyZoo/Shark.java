dcd64633a2b1648871ac1bba8dc79961885ab9df
Benya-pha <128789537+Benya-pha@users.noreply.github.com>
2026-02-18 01:14:09 +0700

    Create Shark.java

---
+++ LAB_7-InstanceVariable&ClassVariable/MyZoo/Shark.java
@@ -0,0 +1,23 @@
+package MyZoo;
+
+public class Shark extends Fish{
+    public Shark() {
+    }
+
+    public Shark(String name) {
+        super(name);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " I have a " + o.getType() + ". I like to " + move() + ". I eat " + eat();
+    }
+
+    public String move(){
+        return "straight swimming" ;
+    }
+
+    public String eat(){
+        return "smallfish";
+    }
+}
