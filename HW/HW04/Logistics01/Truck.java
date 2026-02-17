d9ba19474d231e4550e922ab0a115684c805cd5c
Benya-pha <128789537+Benya-pha@users.noreply.github.com>
2026-02-18 01:44:29 +0700

    Add files via upload

---
+++ HW/HW04/Logistics01/Truck.java
@@ -0,0 +1,13 @@
+package Logistics01;
+
+public class Truck {
+    private double rate = 5;
+
+    public void deliver(){
+        System.out.println("Deliver By Truck");
+    }
+
+    public double getRate(){
+        return rate;
+    }
+}
